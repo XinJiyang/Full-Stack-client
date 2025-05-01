3da87eeb3ce46135cf9db7f7870c09e77721c0ad
Jiyang Xin <62211641+XinJiyang@users.noreply.github.com>
2025-05-01 16:29:14 +1000

    first commit

---
+++ src/App.js
@@ -0,0 +1,86 @@
+import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from "./pages/Home";
+import CreatePost from "./pages/CreatePost";
+import Post from "./pages/Post";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
+import Registration from "./pages/Registration";
+import Profile from "./pages/Profile";
+import ChangePassword from "./pages/ChangePassword";
+
+import { AuthContext } from './helpers/AuthContext';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [authState, setAuthState] = useState({
+    username: "",
+    id: 0,
+    status: false,
+  });
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/auth/auth', {
+      headers: {
+        accessToken: localStorage.getItem('accessToken'),
+      },
+    })
+      .then((response) => {
+        if (response.data.error) {
+          setAuthState((prev) => ({ ...prev, status: false }));
+        } else {
+          setAuthState({
+            username: response.data.username,
+            id: response.data.id,
+            status: true,
+          });
+        }
+      });
+  }, []);
+
+  const Logout = () => {
+    localStorage.removeItem("accessToken");
+    setAuthState({ username: "", id: 0, status: false });
+  };
+
+  return (
+    <div className="App">
+      <AuthContext.Provider value={{ authState, setAuthState }}>
+        <Router>
+          <div className='navbar'>
+            <div className='links'>
+              {!authState.status ? (
+                <>
+                  <Link to="/login">Login</Link>
+                  <Link to="/registration">Registration</Link>
+                </>
+              ) : (
+                <>
+                  <Link to="/">Home</Link>
+                  <Link to="/createpost">Create Post</Link>
+                </>
+              )}
+            </div>
+            <div className='loggedInContainer'>
+              <h1>{authState.username}</h1>
+              {authState.status && <button onClick={Logout}>Logout</button>}
+            </div>
+          </div>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/createpost' element={<CreatePost />} />
+            <Route path='/post/:id' element={<Post />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/profile/:id' element={<Profile />} />
+            <Route path='/changepassword' element={<ChangePassword />} />
+            <Route path='/registration' element={<Registration />} />
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </Router>
+      </AuthContext.Provider>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/PageNotFound.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function PageNotFound() {
+  return (
+    <div>
+      <h1>Page Not Found</h1>
+      <h3>
+        Go to the home page: <a href="/">Home</a>
+      </h3>
+    </div>
+  )
+}
+
+export default PageNotFound
